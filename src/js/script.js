307399e72cd83fd738a08513cbb850cc5a97d9bd
Leonardo dos Santos Cruz <leo.santos199475@gmail.com>
2022-12-30 17:27:27 -0300

    crude versão1.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,12 @@
+let passRows = [];
+      
 
-    
-     // dataBase
-    let passRows = [];
-        
-    // console.log(size)
-
-    
 function start(){
     let size= + document.getElementById('charNum').value
     
     let newPassword = '';
     let character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' ;
     const generateRandomPassword = (size) =>{
-        
         
         for (let i=0;i<size;i++){
             newPassword += character.charAt(Math.floor(Math.random()*character.length));
@@ -23,69 +17,104 @@ function start(){
     }
 
     const addPassRow = ()=>{
-        // document.getElementById('newPass').value = newPassword
+        // 
         const passId = passRows.length +1
         const startDate = document.getElementById('startDate').value;
         const endDate = document.getElementById('endDate').value; 
-        const newPass = newPassword;
+        
     
         passRows.push({
           id: passId,
           startDate: startDate,
           endDate: endDate,
-          password: newPass
+          password: newPassword
         });
 
     }
+    generateRandomPassword(size)
+    addPassRow()
+    renderTable()
    
-    function renderTable() {
-        const table = document.getElementById('dataTable');
-        table.innerHTML = `
-        <table class="table table-dark">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Inicio</th>
-            <th scope="col">Fim</th>
-            <th scope="col">Senha</th>
-            <th scope="col">Ações</th>
-          </tr>
-        </thead>
-      </table>`;
-    
-        passRows.forEach(passRow => {
-          const row = `
-          <tbody>
-            <tr>
-              <td>${passRow.id}</td>
-              <td>${passRow.startDate}</td>
-              <td>${passRow.endDate}</td>
-              <td>${passRow.password}</td>
-              <td>
-                <button class="btn btn-secondary btn-sm" onclick="showEditForm(${passRow.id})">Edit</button>
-                <button class="btn btn-secondary btn-sm" onclick="deleteEmployee(${passRow.id})">Delete</button>
-              </td>
-            </tr>
-          </tbody>
-          `;
-    
-          table.innerHTML += row;
-          
-        });
-        
-    }
-  
-
-
-      generateRandomPassword(size)
-      addPassRow()
-      renderTable();
-       
   }
 
-// add Passwords:
 
-
-    
-    
   
+  function renderTable() {
+    const table = document.getElementById('dataTable');
+    table.innerHTML = `
+    <table class="table table-dark">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Inicio</th>
+        <th scope="col">Fim</th>
+        <th scope="col">Senha</th>
+        <th scope="col">Ações</th>
+      </tr>
+    </thead>
+  </table>`;
+
+    passRows.forEach(passRow => {
+      const row = `
+      <tbody>
+        <tr>
+          <td>${passRow.id}</td>
+          <td>${passRow.startDate}</td>
+          <td>${passRow.endDate}</td>
+          <td>${passRow.password}</td>
+          <td>
+            <button class="btn btn-secondary btn-sm" onclick="showEditForm(${passRow.id})">Edit</button>
+            <button class="btn btn-secondary btn-sm" onclick="deletePassRow(${passRow.id})">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+      `;
+
+      table.innerHTML += row;
+      
+    });
+    
+}
+  
+
+   
+  function showEditForm(id) {
+    const editedPass = passRows.find(passRow => passRow.id == id);
+
+    document.getElementById('edit-id').value = editedPass.id;
+    document.getElementById('edit-start-date').value = editedPass.startDate;
+    document.getElementById('edit-end-date').value = editedPass.endDate;
+    document.getElementById('edit-password').value = editedPass.password;
+    document.getElementById('edit-form').style.display = 'block';
+    
+  }
+  function hideEditForm() {
+    document.getElementById('edit-form').style.display = 'none';
+  }
+
+  function updatePassRow() {
+    //gera novas variaveis buscando no formulário de edição
+  const id = document.getElementById('edit-id').value;
+  const editStartDate = document.getElementById('edit-start-date').value;
+  const editEndDate = document.getElementById('edit-end-date').value;
+  const password = document.getElementById('edit-password').value
+  const passIndex = passRows.findIndex(passRow => passRow.id == id);
+  passRows[passIndex] = { id: id, startDate: editStartDate, endDate: editEndDate, password: password };
+  console.log(passRows[passIndex])
+
+  hideEditForm();
+  renderTable();
+  
+}
+
+  renderTable();
+
+
+  function deletePassRow(id) {
+    passRows = passRows.filter(passRow => passRow.id != id);
+    renderTable();
+  }
+    
+
+
+
